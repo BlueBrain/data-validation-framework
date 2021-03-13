Fix progress bar with multiprocessing computation
Change-Id: I5ef3edb9326a4b8fcccfc9d01e2af429a389dea6

data_validation_framework/util.py
@@ -1,27 +1,55 @@
 """Util functions."""
+import contextlib
 import logging
 import sys
 import traceback
 from itertools import repeat
 from multiprocessing import Pool
+from multiprocessing import Queue
 from multiprocessing import current_process
 
 import numpy as np
 import pandas as pd
 from tqdm.auto import tqdm
+from tqdm.contrib import DummyTqdmFile
 
 L = logging.getLogger(__name__)
+
+tqdm.pandas()
+
+
+@contextlib.contextmanager
+def _std_out_err_redirect_tqdm():
+    """Context manager used to redirect stdout to tqdm.write()."""
+    orig_out_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = map(DummyTqdmFile, orig_out_err)
+        yield orig_out_err[0]
+    # Relay exceptions
+    except Exception as exc:  # pragma: no cover
+        raise exc
+    # Always restore sys.stdout/err if necessary
+    finally:
+        sys.stdout, sys.stderr = orig_out_err
+
+
+def _tqdm_wrapper(*args, **kwargs):
+    _tqdm_wrapper._tqdm_queue.put(1)  # pylint: disable=protected-access
+    return try_operation(*args, **kwargs)
+
+
+def _init_tqdm_global_queue(queue):
+    _tqdm_wrapper._tqdm_queue = queue  # pylint: disable=protected-access
 
 
 def _apply_to_df_internal(data):
     df, args, kwargs = data
     process_name = current_process().name
+    # pylint: disable=no-else-return
     if process_name != "MainProcess":
-        pid = int(current_process().name.split("-")[1])
-        tqdm.pandas(position=pid)
+        return df.apply(_tqdm_wrapper, axis=1, args=args, **kwargs)
     else:
-        tqdm.pandas()
-    return df.progress_apply(try_operation, axis=1, args=args, **kwargs)
+        return df.progress_apply(try_operation, axis=1, args=args, **kwargs)
 
 
 def apply_to_df(df, func, nb_processes, *args, **kwargs):
@@ -31,17 +59,34 @@ def apply_to_df(df, func, nb_processes, *args, **kwargs):
         return _apply_to_df_internal((df, ([func] + list(args)), kwargs))
 
     # Parallel computation
-    nb_chunks = min(len(df), nb_processes)
+    nb_jobs = len(df)
+    nb_chunks = min(nb_jobs, nb_processes)
     chunks = np.array_split(df, nb_chunks)
+    queue = Queue()
 
-    with Pool(nb_processes) as pool:
-        results_list = pool.map(
-            _apply_to_df_internal,
-            zip(chunks, repeat([func] + list(args)), repeat(kwargs)),
-        )
-        pool.close()  # Only needed for coverage
-        pool.join()
-        return pd.concat(results_list)
+    with _std_out_err_redirect_tqdm() as orig_stdout:
+        with Pool(nb_chunks, _init_tqdm_global_queue, [queue]) as pool:
+            # Create the progress bar
+            progress_bar = tqdm(total=nb_jobs, file=orig_stdout, dynamic_ncols=True)
+
+            # Start the computation
+            results_list = pool.imap(
+                _apply_to_df_internal,
+                zip(chunks, repeat([func] + list(args)), repeat(kwargs)),
+            )
+            pool.close()
+
+            # Update progress bar using the Queue
+            for _ in range(nb_jobs):
+                db = queue.get()
+                progress_bar.update(db)
+
+            # Wait for the last jobs to complete
+            pool.join()
+
+    # Gather all results
+    all_res = pd.concat(results_list)
+    return all_res
 
 
 def try_operation(row, func, *args, **kwargs):